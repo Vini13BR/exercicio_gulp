c9cff5b3b39e89b3608b6ac0063a64ee535aef73
Vinicius Costa <vinibr27499@gmail.com>
2025-05-23 17:24:43 -0300

    Adicionando Compressão JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const gulp = require(gulp);
-const sass = require(gulp-sass)(require(sass));
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const gulpUglify = require("gulp-uglify");
+const sourceMap = require("gulp-sourcemap");
+const imagemin = require("gulp-imagemin")
 
 function compilaSass(){
     return gulp.src('./source/styles/main.scss')
-    .pipe(sass())
+    .pipe(sass({
+    outputStyle: 'compressed'
+    }))
+    .pipe(sourceMap.write('./maps'))
     .pipe(gulp.dest('./build/styles'))
 }
 
-exports.default = compilaSass;
+function comprimeJs(){
+    return gulp.src('./source/scripts/*.js')
+        .pipe(gulpUglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+exports.sass = compilaSass;
+exports.js = comprimeJs;
