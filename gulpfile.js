0b6d4e7abe06800f553f20951c72ce9970e4fc21
Vinicius Costa <vinibr27499@gmail.com>
2025-05-23 17:01:11 -0300

    Compilamento Sass Com Gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require(gulp);
+const sass = require(gulp-sass)(require(sass));
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'))
+}
+
+exports.default = compilaSass;
